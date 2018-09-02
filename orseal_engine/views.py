1f30f6c2cf301c197d8ced2a7acd7e4d74299026
James <james@jarofgreen.co.uk>
2018-09-02 10:35:46 +0100

    work

--- orseal_engine/views.py
+++ orseal_engine/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import *
+from django.http import Http404, HttpResponseRedirect, HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello, Open Referral Seal.")
+
+
+
+def project(request, slug):
+    try:
+        project = Project.objects.get(slug=slug)
+    except Project.DoesNotExist:
+        raise Http404("Project does not exist")
+
+    return render(request, 'project/index.html', {
+        'project': project,
+    })
---
+++ test_data.sql
@@ -0,0 +1,6 @@
+
+insert into orseal_engine_project (slug, title) values ('test1', 'Test One');
+
+
+
+
