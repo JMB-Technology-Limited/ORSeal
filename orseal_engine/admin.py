8bdd11b7b2b0a347b3b9ba35d00feec3296f0538
James Baster <james.baster@opendataservices.coop>
2018-09-18 15:28:17 +0100

    register models with admin

--- orseal_engine/admin.py
+++ orseal_engine/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Project)
+admin.site.register(DataOrganization)
+admin.site.register(DataProgram)
+admin.site.register(DataService)
+admin.site.register(DataTaxonomy)
+admin.site.register(DataServiceTaxonomy)
+admin.site.register(DataLocation)
+admin.site.register(DataServiceAtLocation)
+admin.site.register(DataContact)
+admin.site.register(DataPhone)
+admin.site.register(DataPhysicalAddress)
+admin.site.register(DataPostalAddress)
+admin.site.register(DataRegularSchedule)
+admin.site.register(DataHolidaySchedule)
+admin.site.register(DataFunding)
+admin.site.register(DataEligibility)
+admin.site.register(DataServiceArea)
+admin.site.register(DataRequiredDocument)
+admin.site.register(DataPaymentAccepted)
+admin.site.register(DataLanguage)
+admin.site.register(DataAccessibilityForDisabilities)
